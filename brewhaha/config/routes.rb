036fc574fb800575325178c39c033df11688c1a1
William Osler <william@oslers.us>
2015-04-12 21:21:49 -0400

    remove say controller code

--- brewhaha/config/routes.rb
+++ brewhaha/config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'say/hello'
-
-  get 'say/goodbye'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'say#hello'
+  #root 'controller#action'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
