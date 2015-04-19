aaef421faa40efc2a61ab0095de915686df2cf04
Abigail Haseley <haseley.3@osu.edu>
2015-04-19 12:24:20 -0400

    Initial push

--- brewhaha/db/schema.rb
+++ brewhaha/db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150416141959) do
+ActiveRecord::Schema.define(version: 20150419135655) do
+
+  create_table "orders", force: :cascade do |t|
+    t.integer  "quantity",      default: 1,             null: false
+    t.datetime "order_created"
+    t.string   "location",      default: "Dreese Labs", null: false
+    t.datetime "created_at",                            null: false
+    t.datetime "updated_at",                            null: false
+    t.string   "consumer"
+    t.string   "producer"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
