ba2adabc61d75a27fbed2a1fd6bd9148c50a41dd
Abigail Haseley <haseley.3@osu.edu>
2015-04-21 03:36:35 -0400

    Changed forms, there is an error

--- brewhaha/app/controllers/orders_controller.rb
+++ brewhaha/app/controllers/orders_controller.rb
@@ -4,9 +4,7 @@ class OrdersController < ApplicationController
   end
 
   def make_order
-    order = Order.new
-    order.quantity = params[:amount]
-    order.location = params[:location]
+    @order = Order.new
     #order.expiration = params[:expiration]
     #order.consumer_id = current_user.id
   end
